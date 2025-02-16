49d43084a45485c8fdf168f8e8112df8cdcc673b
santiago lopez <slopezarias@usf.edu>
2025-02-16 15:07:27 -0500

    Task 1: Function Declaration

--- cc_7.js
+++ cc_7.js
@@ -1,14 +1,11 @@
 //Task 1
-const calculateInvoice = (subtotal, taxRate, discount) => {
-    let total = (subtotal + (subtotal * taxRate)) - discount;
-    console.log(`Total Invoice: $${total.toFixed(2)}`);   // calculates total invoice after tax and discount
 
+function calculateInvoice(subtotal, taxRate,discount) {
+    let totalAmount = ((subtotal + (subtotal * taxRate)) - discount.toFixed(2));
+    console.log('Total Invoice: $${totalAmount}');
 }
 
-//Task 2
-const calculateHourlyWage = function(salary, hoursPerWeek) {
-    let hourlywage= salary / (hoursPerWeek * 52);
-    console.log (`Hourly wage: $${hourlywage.toFixed(2)}`);  // calculates hourly wage from salary and hours per week
-}
-
+//Test Data
+calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
+calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
 
